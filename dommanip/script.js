a1262380af2c8694052bcb8c6529322559a7892a
Sahana Sripadanna <sripadanna.sahana@gmail.com>
2019-04-18 11:38:34 -0400

    counting h3 tags

---
+++ dommanip/script.js
@@ -0,0 +1,19 @@
+
+
+function getHeaders(){
+
+	var headers = document.getElementsByTagName('h3');
+	alert("there are "  + headers.length + " h3 tags in this document")
+}
+
+function hGreeting(){
+	var div1 = document.querySelector('#div1');
+	var headers = div1.getElementsByTagName('h3');
+	alert("there are "  + headers.length + " h greetings in this division");
+}
+
+function wGreeting(){
+	var div2 = document.querySelector('#div2');
+	var headers = div2.getElementsByTagName('h3');
+	alert("there are "  + headers.length + " w greetings in this division");
+}
